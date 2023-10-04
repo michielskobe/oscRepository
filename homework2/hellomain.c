03984f38346a044809e36c7c8f46a54c4c24d450
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-10-04 20:13:21 +0200

    commit homework2

---
+++ homework2/hellomain.c
@@ -0,0 +1,9 @@
+#include "hellofunc.h"
+
+int main (int argc, char **argv) {
+	// call a function in another file
+	if (argc == 2) myPrintHelloMake (argv [ 1 ]);
+	else myPrintHelloMake ("nobody");
+	return 0;
+}
+
