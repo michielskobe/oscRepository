377a43c23717ef0d547236d1beb00dab4dae13fa
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-10-19 12:23:03 +0200

    sessie2

--- clab2gcc/lab2main.c
+++ clab2gcc/lab2main.c
@@ -1,24 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-typedef struct {
-    short day, month;
-    unsigned year;
-} date_t;
 
-void f( void ) {
-    int x, y, z;
-    printf("%d %d %d\n", x, y, z );
+void swap_pointer(void*, void*);
+
+void swap_pointer(void*, void*) {
+    return;
 }
-date_t * date_struct( int day, int month, int year ) {
-    date_t dummy;
-    dummy.day = (short)day;
-    dummy.month = (short)month;
-    dummy.year = (unsigned)year;
-    return &dummy;
-}
+
+
 int main( void ) {
-    int day, month, year;
-    date_t *d;
-    printf("\nGive day, month, year:");
-    scanf("%d %d %d", &day, &month, &year);
 
+    return 0;
+}
