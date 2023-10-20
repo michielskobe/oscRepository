864b39629ae3d53883f3596b2882543a47a6b3d7
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-10-20 15:12:26 +0200

    sessie 2

--- clab2gcc/lab2exercise5.c
+++ clab2gcc/lab2exercise5.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 #define FREQ 1;
 #define MIN_TEMP = -10;
 #define MAX_TEMP = 35;
 
 int main( void ) {
+    int temperature;
+    temperature =
+
 
     return 0;
 }
