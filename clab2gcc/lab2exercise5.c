e296763b1f73d5e48c7981495ac17dc119d0d16c
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-10-24 00:42:24 +0200

    lab2 exercise 5 + start debug exercise

--- clab2gcc/lab2exercise5.c
+++ clab2gcc/lab2exercise5.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
-
-#define FREQ 1;
-#define MIN_TEMP = -10;
-#define MAX_TEMP = 35;
+#define FREQ 2
+#define MIN_TEMP (-10)
+#define MAX_TEMP 35
 
 int main( void ) {
     int temperature;
-    temperature =
-
-
-    return 0;
+    srand(time(NULL));
+    for (;;) {
+        temperature = (rand() % (MAX_TEMP-MIN_TEMP) + MIN_TEMP);
+        struct tm* local;
+        time_t t = time(NULL);
+        local = localtime(&t);
+        char* time = asctime(local);
+        printf("Temperature = %d°C @%s",temperature,time);
+        fflush(stdout);
+        sleep(FREQ);
+    }
+  return 0;
 }
