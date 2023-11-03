1d47465901bb4c536204b727cfd4af3faec5edbf
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-11-03 21:04:50 +0100

    milestone1 update

--- clab3gcc/milestone1/dplist_test.c
+++ clab3gcc/milestone1/dplist_test.c
@@ -65,28 +65,37 @@ void yourtest1()
         ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
 
         // TODO : your test scenarios
-
-        list = dpl_create(element_copy, element_free, element_compare);
         my_element_t *content1 = (my_element_t *)malloc(sizeof(my_element_t));
         content1->id = 5;
         content1->name = "foo";
-        dpl_insert_at_index(list, content1, 0, true);
-        ck_assert_msg(dpl_size(list) == 1, "Numbers must contain 1 element.");
 
         my_element_t *content2 = (my_element_t *)malloc(sizeof(my_element_t));
         content2->id = 3;
         content2->name = "boo";
-        dpl_insert_at_index(list, content2, -1, true);
-        ck_assert_msg(dpl_size(list) == 2, "Numbers must contain 2 elements.");
 
         my_element_t *content3 = (my_element_t *)malloc(sizeof(my_element_t));
         content3->id = 2;
         content3->name = "hoo";
+
+        int *content4 = malloc(sizeof(int));
+        *content4 = 5;
+
+        list = dpl_create(element_copy, element_free, element_compare);
+
+        dpl_insert_at_index(list, content1, 0, true);
+        ck_assert_msg(dpl_size(list) == 1, "Numbers must contain 1 element.");
+
+        dpl_insert_at_index(list, content2, -1, true);
+        ck_assert_msg(dpl_size(list) == 2, "Numbers must contain 2 elements.");
+
         dpl_insert_at_index(list, content3, 99, true);
         ck_assert_msg(dpl_size(list) == 3, "Numbers must contain 3 elements.");
 
         dpl_remove_at_index(list, 2, true);
         ck_assert_msg(dpl_size(list) == 2, "Numbers must contain 2 elements.");
+
+        dpl_insert_at_index(list, content4, 6, true);
+        ck_assert_msg(dpl_size(list) == 3, "Numbers must contain 3 elements.");
 
         dpl_free(&list, true);
 
@@ -102,7 +111,19 @@ void yourtest1()
 
         dpl_remove_at_index(list, 2, false);
         ck_assert_msg(dpl_size(list) == 2, "Numbers must contain 2 elements.");
-        
+
+        dplist_node_t *node = (dplist_node_t *) dpl_get_reference_at_index(list, 1);
+        my_element_t *el = (my_element_t *) dpl_get_element_at_reference(list, node);
+        ck_assert_msg(el == content1, "Failure: expected element at 1 is content1");
+
+        my_element_t *non_existing_el = (my_element_t *) dpl_get_element_at_reference(list, (dplist_node_t *) 0x5555555593a1);
+        ck_assert_msg(non_existing_el == NULL, "Failure: expected result to be NULL");
+
+        ck_assert_msg(dpl_get_index_of_element(list, content1) == 1, "Failure: expected result to be 1");
+        ck_assert_msg(dpl_get_index_of_element(list, content3) == -1, "Failure: expected result to be -1");
+
+        dpl_insert_at_index(list, content4, 6, false);
+        ck_assert_msg(dpl_size(list) == 3, "Numbers must contain 3 elements.");
 
         dpl_free(&list, false);
 
