67bb3b992ccb0e8460fc904cc00903c1daa1e1b1
Kobe Michiels <kobe.michiels2@student.kuleuven.be>
2023-12-13 00:14:13 +0100

    Restructured datamgr.c and freed allocated memory resources

--- projectLearningLabs/final_project/datamgr.c
+++ projectLearningLabs/final_project/datamgr.c
@@ -53,23 +53,23 @@ int element_compare(void * x, void * y) {
 }
 
 
-int data_log_fd;
-char data_log_msg[SIZE];
-sbuffer_t *data_buffer;
+int fd_datamgr;
+char log_msg_datamgr[SIZE];
+sbuffer_t *buffer_datamgr;
 dplist_t *sensor_list;
 
 void *data_manager(void *arg) {
-    data_param_t *data_param = arg;
-    data_buffer = data_param->buffer;
-    data_log_fd = data_param->write_end;
+    data_thread_arg_t *data_thread_arg = arg;
+    buffer_datamgr = data_thread_arg->buffer;
+    fd_datamgr = data_thread_arg->fd;
 
+    // create data list for sensors
     sensor_list = dpl_create(element_copy, element_free, element_compare);
-    uint16_t sensor_id, room_id, data_sensor_id;
-    double temperature;
-    time_t timestamp;
+
+    // fill list with elements containing sensor and room id's
+    uint16_t sensor_id, room_id;
     FILE *fp_sensor_map = fopen("room_sensor.map", "r");
     my_element_t *sensor = malloc(sizeof(my_element_t));
-    //insert sensor and room id's into list
     while(!feof(fp_sensor_map)) {
         fscanf(fp_sensor_map, "%hd %hd\n", &room_id, &sensor_id);
         assert(sensor != NULL);
@@ -79,54 +79,59 @@ void *data_manager(void *arg) {
         dpl_insert_at_index(sensor_list,sensor, dpl_size(sensor_list),true);
     }
     fclose(fp_sensor_map);
+
+    // insert buffer data at corresponding node of sensor list
     sensor_data_t *data = malloc(sizeof(sensor_data_t));
-    //insert buffer data at corresponding sensor node
-    while(sbuffer_read(data_buffer, data) != SBUFFER_NO_DATA) {
-        // Read data of 1 sensor
-        data_sensor_id = data->id;
+    uint16_t buffer_data_id;
+    double temperature;
+    time_t timestamp;
+    while(sbuffer_read(buffer_datamgr, data) != SBUFFER_NO_DATA) {
+        buffer_data_id = data->id;
         temperature = data->value;
         timestamp = data->ts;
-        dplist_node_t *sensor_node = datamgr_get_sensor_with_id(data_sensor_id);
+        dplist_node_t *sensor_node = get_sensor_with_id(buffer_data_id);
         if (sensor_node != NULL) {
-            my_element_t *sensor = sensor_node->element;
-            int index = (sensor->temperature_count)%RUN_AVG_LENGTH;
+            // calculate running average
+            my_element_t *sensor_element = sensor_node->element;
+            int index = (sensor_element->temperature_count)%RUN_AVG_LENGTH;
             if (index < RUN_AVG_LENGTH) {
-                sensor->temperatures[index] = temperature;
-                (sensor->temperature_count)++;
-                if (sensor->temperature_count >= RUN_AVG_LENGTH) {
+                sensor_element->temperatures[index] = temperature;
+                (sensor_element->temperature_count)++;
+                if (sensor_element->temperature_count >= RUN_AVG_LENGTH) {
                     // Calculate running average:
                     double temperature_sum = 0;
                     for (int i = 0; i < RUN_AVG_LENGTH; i++) {
-                        temperature_sum += sensor->temperatures[i];
+                        temperature_sum += sensor_element->temperatures[i];
                     }
-                    sensor->running_avg = (temperature_sum/RUN_AVG_LENGTH);
-                    sensor->last_modified = timestamp;
+                    sensor_element->running_avg = (temperature_sum/RUN_AVG_LENGTH);
+                    sensor_element->last_modified = timestamp;
                 }
             }
             else {
                 ERROR_HANDLER((index >= RUN_AVG_LENGTH), "Index out of bounds");
             }
-            //printf("Sensor ID: %hd | Room ID: %hd | Running average: %.2f°C | Last modification: %ld\n", sensor->sensor_id, sensor->room_id, sensor->running_avg, sensor->last_modified);
-            if (sensor->running_avg < SET_MIN_TEMP && sensor->temperature_count >= RUN_AVG_LENGTH) {
-                //fprintf( stderr, "Too cold in room %hd\n",sensor->room_id);
-                sprintf(data_log_msg, "Sensor node %" PRIu16 " reports it’s too cold (avg temp = %f)", sensor->sensor_id, sensor->running_avg);
-                write(data_log_fd, data_log_msg, SIZE);
+            // internal decision-making based on running average and temperature bounds
+            if (sensor_element->running_avg < SET_MIN_TEMP && sensor_element->temperature_count >= RUN_AVG_LENGTH) {
+                sprintf(log_msg_datamgr, "Sensor node %" PRIu16 " reports it’s too cold (avg temp = %f)", sensor_element->sensor_id, sensor_element->running_avg);
+                write(fd_datamgr, log_msg_datamgr, SIZE);
             }
-            else if (sensor->running_avg > SET_MAX_TEMP && sensor->temperature_count >= RUN_AVG_LENGTH) {
-                //fprintf(stderr, "Too hot in room %hd\n",sensor->room_id);
-                sprintf(data_log_msg, "Sensor node %" PRIu16 " reports it’s too hot (avg temp = %f)", sensor->sensor_id, sensor->running_avg);
-                write(data_log_fd, data_log_msg, SIZE);
+            else if (sensor_element->running_avg > SET_MAX_TEMP && sensor_element->temperature_count >= RUN_AVG_LENGTH) {
+                sprintf(log_msg_datamgr, "Sensor node %" PRIu16 " reports it’s too hot (avg temp = %f)", sensor_element->sensor_id, sensor_element->running_avg);
+                write(fd_datamgr, log_msg_datamgr, SIZE);
             }
         } else {
-            //fprintf( stderr, "Data from unknown sensor %hd is ignored.\n",data_sensor_id);
-            sprintf(data_log_msg, "Received sensor data with invalid sensor node ID %" PRIu16 " ", data_sensor_id);
-            write(data_log_fd, data_log_msg, SIZE);
+            sprintf(log_msg_datamgr, "Received sensor data with invalid sensor node ID %" PRIu16 " ", buffer_data_id);
+            write(fd_datamgr, log_msg_datamgr, SIZE);
         }
     }
+    // free allocated memory resources
+    free(sensor);
+    free(data);
+    dpl_free(&sensor_list, true);
     return 0;
 }
 
-dplist_node_t *datamgr_get_sensor_with_id(sensor_id_t id) {
+dplist_node_t *get_sensor_with_id(sensor_id_t id) {
     dplist_node_t *current_node = sensor_list->head;
     while(current_node != NULL){
         uint16_t current_node_id = ((my_element_t*)current_node->element)->sensor_id;
